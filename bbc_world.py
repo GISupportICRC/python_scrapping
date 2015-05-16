9a282c0987913ea9dd060ec15f6c43fd6fc37c1e
pierre <pierrelorioux@hotmail.fr>
2015-05-16 21:41:58 +0200

    Update bbc_world.py

--- bbc_world.py
+++ bbc_world.py
@@ -5,13 +5,13 @@ import requests
 
 outfile = 'bbc_world.csv'
 
-# To do :
+# To do 
 # Hide the user for the geoname API
 # geonameuser = raw_input('ICRC username')
 # icrcopassist
 
+# Create a new file for everyday !
 # Get the country name based on spatial join with countries geojson.
-# Create a new file for everyday share it on Git ?
 
 with open(outfile, 'w+') as csvfile:
     fieldnames = ['title','url','desc','pubdate','lat','lng']
@@ -28,7 +28,7 @@ with open(outfile, 'w+') as csvfile:
 
     for i, element in enumerate(soup):
         elmt_item = element.findChildren()
-        if len(eltitem) >1:
+        if len(elmt_item) >1:
             # a clean file !
             itm1 = elmt_item[0]
 
@@ -43,19 +43,17 @@ with open(outfile, 'w+') as csvfile:
     for i in item2:
         info = str(i)
         soup3 = BeautifulSoup(info)
-        
-        #Finally getting the values here
+
         title  = soup3.find_all('title')
         url = soup3.find_all('guid')
         desc = soup3.find_all('description')
         pubdate = soup3.find_all('pubdate')
         lat  = soup3.find_all('geo:lat')
         lng = soup3.find_all('geo:long')
-    
-        #boring data cleaning.. Maybe a better way to do it ?
+
         title_clean = str(title).replace('<title>','').replace('</title>','').replace('[','').replace(']','')
         url_clean = str(url).replace('<guid ispermalink="false">','').replace('</guid>','').replace('[','').replace(']','')
-        desc_clean =str(desc).replace('<description>','').replace('</description>','').replace('[','').replace(']','')
+        desc_clean =str(desc).replace('<description>','').replace('</description>','').replace('[','').replace(']','').replace('"','')
         pubdate_clean = str(pubdate).replace('<pubdate>','').replace('</pubdate>','').replace('[','').replace(']','')
         lat_clean = str(lat).replace('<geo:lat>','').replace('</geo:lat>','').replace('[','').replace(']','')
         lng_clean = str(lng).replace('<geo:long>','').replace('</geo:long>','').replace('[','').replace(']','')
