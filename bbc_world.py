7e457f0a0db1c0eff819b0c73e6c21e04d47e7a2
pierre <pierrelorioux@hotmail.fr>
2015-05-28 09:41:04 +0200

    Update bbc_world.py

--- bbc_world.py
+++ bbc_world.py
@@ -6,40 +6,28 @@ import requests
 
 outfile = 'bbc_world.csv'
 
-# To do 
-# Hide the user for the geoname API
-# geonameuser = raw_input('ICRC username')
-# icrcopassist
-#pour geoloc les coordinates
-#http://api.geonames.org/countryCode?lat=47.03&lng=10.2&username=demo 
-
-# Create a new file for everyday !
-# Get the country name based on spatial join with countries geojson.
-
 with open(outfile, 'wb') as csvfile:
-    fieldnames = ['title','url','desc','pubdate','lat','lng']
+    
+	fieldnames = ['title','url','desc','pubdate','lat','lng']
     writer = csv.DictWriter(csvfile, delimiter=',',fieldnames=fieldnames)
 
     writer.writeheader()
     r  = requests.get("http://api.geonames.org/rssToGeoRSS?feedUrl=http://news.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml&username=icrcopassist")
     data = r.text
-
-    #premiere soupe
+	
     soup = BeautifulSoup(r.content, ['lxml', 'xml'])
     soup_list = soup.findChildren()
-
+	
+	#Getting a clean file out off the rss mess
     for i, element in enumerate(soup):
         elmt_item = element.findChildren()
         if len(elmt_item) >1:
             # a clean file !
             itm1 = elmt_item[0]
-
-    #strigified to be placed as a new soup
-    itm = str(itm1)
-
-    #nouvelle soupe
+    
+	#Working now on this clean file
+	itm = str(itm1)
     soup2 = BeautifulSoup(itm)
-    #Now we're looking for the items in the soup to iterate on each of them and write the rows.
     item2 = soup2.find_all("item")
 
     for i in item2:
@@ -52,7 +40,8 @@ with open(outfile, 'wb') as csvfile:
         pubdate = soup3.find_all('pubdate')
         lat  = soup3.find_all('geo:lat')
         lng = soup3.find_all('geo:long')
-
+		
+		#Cleaning the result
         title_clean = str(title).replace('<title>','').replace('</title>','').replace('[','').replace(']','').replace(',','')
         url_clean = str(url).replace('<guid ispermalink="false">','').replace('</guid>','').replace('[','').replace(']','')
         desc_clean =str(desc).replace('<description>','').replace('</description>','').replace('[','').replace(']','').replace('"','').replace(',','')
@@ -61,5 +50,6 @@ with open(outfile, 'wb') as csvfile:
         lng_clean = str(lng).replace('<geo:long>','').replace('</geo:long>','').replace('[','').replace(']','')
 
         #Write the code to get the country here !
-
+		
+		#For each row:
         writer.writerow({'desc':desc,'title':title_clean,'url':url_clean,'desc':desc_clean,'pubdate':pubdate_clean,'lat':lat_clean,'lng':lng_clean})
